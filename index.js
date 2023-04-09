deb6e2e691e391e4df32c3da1b4d9371c97abfad
Scott Schwab <scottschwab86@gmail.com>
2023-04-09 10:16:52 -0500

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(arr) {
+  let wonSuperbowlObj = arr.find((obj) => {
+    return obj.result === 'W';
+  });
+
+  return wonSuperbowlObj ? wonSuperbowlObj.year : undefined;
+
+}
